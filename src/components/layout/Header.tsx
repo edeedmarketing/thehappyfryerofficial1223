2ab87ee9a5d544863fb77318ed706063e5919d37
Westbrook00 <info@edeed.com.au>
2025-11-12 14:39:06 +1000

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -40,8 +40,8 @@ const Header = () => {
 
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ease-in-out ${headerClasses}`}>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className={`flex items-center py-3 ${shouldBeTransparent ? 'justify-between' : 'justify-between'}`}>
+      <div className={`${shouldBeTransparent ? 'w-full px-4 sm:px-6 lg:px-8' : 'max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'}`}>
+        <div className="flex items-center justify-between py-3">
           {!shouldBeTransparent && (
             <Link to="/" className="flex items-center space-x-2">
               <img
@@ -52,7 +52,7 @@ const Header = () => {
             </Link>
           )}
 
-          <nav className="hidden lg:flex items-center space-x-8">
+          <nav className={`hidden lg:flex items-center space-x-8 ${shouldBeTransparent ? 'ml-0' : ''}`}>
             {navLinks.map((link) => (
               <Link
                 key={link.path}
