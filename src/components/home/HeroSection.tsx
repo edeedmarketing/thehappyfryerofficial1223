6788fd8775c90eb16728198c71344f040411426f
Westbrook00 <info@edeed.com.au>
2025-11-12 16:11:44 +1000

    Updated HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -41,9 +41,9 @@ const HeroSection = () => {
       {/* Logo */}
       <div className="relative z-10 text-center">
         <div className="transform hover:scale-110 transition-transform duration-500 animate-zoom-in">
-          <img 
-            src="/happy_fryer_transparent.png" 
-            alt="The Happy Fryer Logo" 
+          <img
+            src="/files_5754595-1762927727709-happy_fryer_transparent.png"
+            alt="The Happy Fryer Logo"
             className="w-32 h-32 md:w-48 md:h-48 mx-auto drop-shadow-2xl"
           />
         </div>
