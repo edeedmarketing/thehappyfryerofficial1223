ae5a474c683b1622a7f616dec4e4e6c38d73ace2
Westbrook00 <info@edeed.com.au>
2025-11-12 15:00:23 +1000

    Updated index.css

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection = () => {
         <img
           src="/happy_fryer_transparent.png"
           alt="The Happy Fryer"
-          className="w-48 h-48 sm:w-56 sm:h-56 md:w-64 md:h-64 object-contain transition-transform duration-300 hover:scale-110 cursor-pointer"
+          className="w-48 h-48 sm:w-56 sm:h-56 md:w-64 md:h-64 object-contain transition-transform duration-300 hover:scale-110 cursor-pointer animate-zoom-in"
         />
       </div>
     </section>
