c605bc2d8d739e5ff1957d70c4122813f4199812
Westbrook00 <info@edeed.com.au>
2025-11-12 16:12:49 +1000

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import HeroSection from '../components/home/HeroSection';
-import LocationHours from '../components/home/LocationHours';
 import AboutSection from '../components/home/AboutSection';
 import SeafoodMore from '../components/home/SeafoodMore';
 import ReviewsSection from '../components/home/ReviewsSection';
@@ -11,7 +10,6 @@ const Home = () => {
   return (
     <div>
       <HeroSection />
-      <LocationHours />
       <AboutSection />
       <SeafoodMore />
       <ReviewsSection />
