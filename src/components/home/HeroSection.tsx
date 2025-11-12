d0b26dcb93dda9b47ca4348a07bb96d52186dfe0
Westbrook00 <info@edeed.com.au>
2025-11-12 14:48:51 +1000

    Updated HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -10,6 +10,13 @@ const HeroSection = () => {
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       </div>
 
+      <div className="relative z-10 flex items-center justify-center">
+        <img
+          src="/happy_fryer_transparent.png"
+          alt="The Happy Fryer"
+          className="w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 object-contain"
+        />
+      </div>
     </section>
   );
 };
